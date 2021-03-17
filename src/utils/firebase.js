d1622acd8c5e957ad00a961b513e94aaf8be6cde
JonesDev <youness.agnaou@outlook.com>
2021-03-17 12:22:48 +0100

    first commit done

---
+++ src/utils/firebase.js
@@ -0,0 +1,16 @@
+  import firebase from 'firebase/app'
+  import 'firebase/auth';
+
+  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {};
+
+
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+
+  //exporter notre base de données
+  export default fireDB;
+
+  //export the auth app
+  export const auth = app.auth();
